7b8197a82a9f93a487e17d57ed89ba496b6090da
your name <nguyenthienthuan409@gmail.com>
2024-10-27 20:43:21 +0700

    Connect to the local database.

--- DAO/Database.cs
+++ DAO/Database.cs
@@ -19,11 +19,12 @@ namespace DAO
 
         public string serverName = @"iris523.database.windows.net";
         public string dbName = "QLKS";
-        public string userName = "nhom8";
-        public string password = "Gohomeafter10pm";
+        // public string userName = "nhom8";
+        // public string password = "Gohomeafter10pm";
         public Database()
         {
-            string stringConnect = "Server=" + serverName + "; Database=" + dbName + "; User Id=" + userName + "; Password=" + password + ";"; 
+            string stringConnect = "Server=" + serverName + "; Database=" + dbName + "; Integrated Security=True;";
+            // string stringConnect = "Server=" + serverName + "; Database=" + dbName + "; User Id=" + userName + "; Password=" + password + ";"; 
             conn = new SqlConnection(stringConnect);
         }
 

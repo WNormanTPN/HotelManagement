d3b3a9d7d7f06a5f54a5ac33c71f0d06f5945374
Norman <vinhstarmcpc@gmail.com>
2024-10-27 20:41:06 +0700

    Add: forgot password feature

--- GUI/frmForgotPassword.cs
+++ GUI/frmForgotPassword.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using BUS;
+using DTO;
+using Google.Apis.Auth.OAuth2;
+using GUI.GUI_COMPONENT;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -71,5 +78,65 @@ namespace GUI
             login.ShowDialog();
             this.Close();
         }
+
+        private async void buttonRounded3_Click(object sender, EventArgs e)
+        {
+            TaiKhoanBUS tkBUS = new TaiKhoanBUS();
+            TaiKhoanDTO tkDTO = tkBUS.GetTK(txtTK.Text);
+
+            if (!string.IsNullOrEmpty(tkDTO.TaiKhoan))
+            {
+                NhanVienBUS nvBUS = new NhanVienBUS();
+                NhanVienDTO nvDTO = nvBUS.GetNV(tkDTO.MaNV);
+                string email = nvDTO.Email;
+
+                try
+                {
+                    // Tạo nội dung email
+                    var message = new MimeMessage();
+                    message.From.Add(new MailboxAddress("Tên của bạn", "your_email@gmail.com"));
+                    message.To.Add(new MailboxAddress("Tên người nhận", email));
+                    message.Subject = "Thông báo quan trọng từ hệ thống";
+                    message.Body = new TextPart("plain")
+                    {
+                        Text = "Đây là nội dung email thông báo về tài khoản của bạn."
+                    };
+
+                    // Đường dẫn tới file JSON đã tải từ Google Cloud Console
+                    var credentialPath = "path/to/your/client_secret.json";
+                    var clientSecrets = GoogleClientSecrets.Load(File.OpenRead(credentialPath)).Secrets;
+
+                    // Xác thực OAuth2
+                    var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        clientSecrets,
+                        new[] { "https://mail.google.com/" },
+                        "user",
+                        CancellationToken.None);
+
+                    using (var client = new SmtpClient())
+                    {
+                        await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+
+                        // Sử dụng OAuth2 để xác thực và gửi email
+                        var oauth2 = new SaslMechanismOAuth2("your_email@gmail.com", credential.Token.AccessToken);
+                        await client.AuthenticateAsync(oauth2);
+                        await client.SendAsync(message);
+                        await client.DisconnectAsync(true);
+                    }
+
+                    MessageBox.Show("Email đã được gửi thành công!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gửi email thất bại: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBoxDialog message = new MessageBoxDialog();
+                message.ShowDialog("Lỗi", "", "Không tìm thấy tài khoản", MessageBoxDialog.ERROR, MessageBoxDialog.YES, "Có", "", "");
+            }
+        }
+
     }
 }
